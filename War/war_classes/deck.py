dff2a1e84dd2e2b637fc7cb67b4f2fdf7c22be3e
Shailya Gandhi <shailya.gandhi100@gmail.com>
2025-08-06 21:06:14 +0530

    Update deck.py

--- War/war_classes/deck.py
+++ War/war_classes/deck.py
@@ -20,23 +20,24 @@ class Deck:
     def deal_one_card(self):
         return self.all_cards.pop()
 
+# Un-Comment The Following Code to Test The Module Independently.
+# if __name__ == '__main__':
+#     print('Testing Deck Class:')
+#     print('=' * 100)
 
-if __name__ == '__main__':
-    print('Testing Deck Class:')
-    print('=' * 100)
+#     d1 = Deck()
+#     for card in d1.all_cards:
+#         print(card)
 
-    d1 = Deck()
-    for card in d1.all_cards:
-        print(card)
+#     print('=' * 100)
 
-    print('=' * 100)
+#     d1.shuffle_deck()
+#     for card in d1.all_cards:
+#         print(card)
 
-    d1.shuffle_deck()
-    for card in d1.all_cards:
-        print(card)
+#     temp = d1.deal_one_card()
+#     print(temp)
+#     print(temp.rank)
+#     print(temp.suit)
 
-    temp = d1.deal_one_card()
-    print(temp)
-    print(temp.rank)
-    print(temp.suit)
-    print(temp.value)
+#     print(temp.value)
